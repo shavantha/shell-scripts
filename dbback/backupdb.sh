c2dbcc27c5b0985b59ff3113cb72729918532a6f
Shavantha Weerasinghe <shavantha@gmail.com>
2017-06-28 09:51:57 +0000

    updated the paths

--- dbback/backupdb.sh
+++ dbback/backupdb.sh
@@ -2,8 +2,8 @@
 
 DATE=`date +"%Y-%m-%d"`
 
-mysqldump -udbuser -pdbuser dvwa > /home/shavantha/products/software/hack/dbbackups/dvwa_backup_$DATE.sql
+mysqldump -udbuser -pdbuser mydb > /home/shavantha/products/software/dbbackups/mydb_backup_$DATE.sql
 
 REM_DATE=`date --date="30 days ago" +"%Y-%m-%d"`
-rm /home/shavantha/products/software/hack/dbbackups/dvwa_backup_$REM_DATE.sql
+rm /home/shavantha/products/software/dbbackups/mydb_backup_$REM_DATE.sql
 ~                                                                   
